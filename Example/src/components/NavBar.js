12b525c07332f161636722e3aeae27fb48367b90
Hein Rutjes <hrutjes@gmail.com>
2019-06-18 10:08:18 +0200

    NavBar styling updates

--- Example/src/components/NavBar.js
+++ Example/src/components/NavBar.js
@@ -1,50 +1,61 @@
 // @flow
-import * as React from 'react';
-import { StyleSheet, View, TouchableOpacity } from 'react-native';
-import { Heading2 } from './Text';
-import { Router } from './Router';
+import * as React from "react";
+import { StyleSheet, View, TouchableOpacity } from "react-native";
+import { Heading2 } from "./Text";
+import { Router } from "./Router";
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'flex-end',
-    height: 80,
-    backgroundColor: '#EEEEEE',
-    paddingHorizontal: 16,
-    paddingBottom: 16,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "flex-end",
+    height: 86,
+    backgroundColor: "#EEEEEE",
+    paddingHorizontal: 32,
+    paddingBottom: 12
   },
   backContainer: {
-    position: 'absolute',
-    left: 16,
-    bottom: 16,
-  },
+    position: "absolute",
+    left: 24,
+    bottom: 16
+  }
 });
 
 export interface NavBarProps {
   title: string;
-  back: 'default' | 'none' | 'close';
+  back: "default" | "none" | "close";
 }
+
+const HIT_SLOP = {
+  left: 16,
+  top: 16,
+  right: 16,
+  bottom: 16
+};
 
 export class NavBar extends React.Component<NavBarProps> {
   static defaultProps = {
-    back: 'default',
+    back: "default"
   };
 
   renderBack() {
     let label;
     switch (this.props.back) {
-      case 'none':
+      case "none":
         return;
-      case 'default':
-        label = '<';
+      case "default":
+        label = "<";
         break;
-      case 'close':
-        label = 'X';
+      case "close":
+        label = "X";
         break;
     }
     return (
-      <TouchableOpacity style={styles.backContainer} onPress={this.onPressBack}>
+      <TouchableOpacity
+        style={styles.backContainer}
+        onPress={this.onPressBack}
+        hitSlop={HIT_SLOP}
+      >
         <View>
           <Heading2>{label}</Heading2>
         </View>
