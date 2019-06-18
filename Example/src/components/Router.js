c19cc2161bf1cd54ee83d00e0a93d8c9dad19b5b
Hein Rutjes <hrutjes@gmail.com>
2019-06-18 09:26:47 +0200

    Added preliminary Back gesture handling to Example app

--- Example/src/components/Router.js
+++ Example/src/components/Router.js
@@ -1,20 +1,30 @@
 // @flow
-import * as React from 'react';
-import { StyleSheet, View, Animated, Dimensions } from 'react-native';
-import { SharedElementTransition } from 'react-native-shared-element-transition';
-import type { SharedElementSourceRef } from 'react-native-shared-element-transition';
-import { ScreenTransitionContext } from './ScreenTransitionContext';
-import type { ScreenTransitionContextOnSharedElementsUpdatedEvent } from './ScreenTransitionContext';
+import * as React from "react";
+import { StyleSheet, View, Animated, Dimensions } from "react-native";
+import { SharedElementTransition } from "react-native-shared-element-transition";
+import type { SharedElementSourceRef } from "react-native-shared-element-transition";
+import { ScreenTransitionContext } from "./ScreenTransitionContext";
+import type { ScreenTransitionContextOnSharedElementsUpdatedEvent } from "./ScreenTransitionContext";
+import { PanGestureHandler, State } from "react-native-gesture-handler";
 
-const WIDTH = Dimensions.get('window').width;
+const WIDTH = Dimensions.get("window").width;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1
   },
   node: {
-    ...StyleSheet.absoluteFillObject,
+    ...StyleSheet.absoluteFillObject
   },
+  swipeBackOverlay: {
+    position: "absolute",
+    left: 0,
+    top: 0,
+    bottom: 0,
+    width: 20
+    //backgroundColor: "green",
+    //opacity: 0.2
+  }
 });
 
 interface RouterProps {
@@ -25,15 +35,22 @@ interface RouterState {
   stack: React.Node[];
   prevIndex: number;
   nextIndex: number;
-  animValue: Animated.Value;
+  animValue: Animated.Node;
   sharedElementSources: Array<{
-    [string]: SharedElementSourceRef,
+    [string]: SharedElementSourceRef
   }>;
 }
 
 let router;
 
 export class Router extends React.Component<{}, RouterState> {
+  _animValue = new Animated.Value(0);
+  _swipeBackAnimValue = new Animated.Value(0);
+  _onSwipeBackGestureEvent = Animated.event(
+    [{ nativeEvent: { x: this._swipeBackAnimValue } }],
+    { useNativeDriver: true }
+  );
+
   constructor(props: RouterProps) {
     super(props);
     router = this;
@@ -41,13 +58,22 @@ export class Router extends React.Component<{}, RouterState> {
       stack: [props.initialNode],
       prevIndex: 0,
       nextIndex: 0,
-      animValue: new Animated.Value(0),
-      sharedElementSources: [],
+      animValue: Animated.subtract(
+        this._animValue,
+        Animated.divide(this._swipeBackAnimValue, WIDTH)
+      ),
+      sharedElementSources: []
     };
   }
 
   renderSharedElementTransitions() {
-    const { prevIndex, nextIndex, stack, sharedElementSources, animValue } = this.state;
+    const {
+      prevIndex,
+      nextIndex,
+      stack,
+      sharedElementSources,
+      animValue
+    } = this.state;
     if (prevIndex === nextIndex && nextIndex === stack.length - 1) {
       // console.log('renderSharedElementTransitions: null');
       return null;
@@ -58,7 +84,8 @@ export class Router extends React.Component<{}, RouterState> {
     for (let index = startIndex; index < endIndex; index++) {
       const sceneSources = sharedElementSources[index];
       for (let sharedId in sceneSources) {
-        sources[sharedId] = sources[sharedId] || new Array(endIndex - startIndex);
+        sources[sharedId] =
+          sources[sharedId] || new Array(endIndex - startIndex);
         sources[sharedId][index - startIndex] = sceneSources[sharedId];
       }
     }
@@ -77,6 +104,62 @@ export class Router extends React.Component<{}, RouterState> {
     );
   }
 
+  renderBackSwiper() {
+    const { nextIndex, prevIndex } = this.state;
+    if (!nextIndex && !prevIndex) return;
+    return (
+      <PanGestureHandler
+        onGestureEvent={this._onSwipeBackGestureEvent}
+        onHandlerStateChange={this._onSwipeBackStateChange}
+      >
+        <Animated.View style={styles.swipeBackOverlay} collapsable={false} />
+      </PanGestureHandler>
+    );
+  }
+
+  _onSwipeBackStateChange = (event: any) => {
+    const { nativeEvent } = event;
+    switch (nativeEvent.state) {
+      case State.ACTIVE:
+        this.setState({
+          nextIndex: this.state.nextIndex - 1
+        });
+        break;
+      case State.CANCELLED:
+        this.setState({
+          nextIndex: this.state.prevIndex
+        });
+        break;
+      case State.END:
+        if (nativeEvent.translationX >= WIDTH / 2) {
+          Animated.timing(this._swipeBackAnimValue, {
+            toValue: WIDTH,
+            duration: 100,
+            useNativeDriver: true
+          }).start(({ finished }) => {
+            if (finished) {
+              this.pruneStack(this.state.nextIndex + 1);
+              this._swipeBackAnimValue.setValue(0);
+              this._animValue.setValue(this.state.nextIndex);
+            }
+          });
+        } else {
+          Animated.timing(this._swipeBackAnimValue, {
+            toValue: 0,
+            duration: 100,
+            useNativeDriver: true
+          }).start(({ finished }) => {
+            if (finished) {
+              this.setState({
+                nextIndex: this.state.prevIndex
+              });
+            }
+          });
+        }
+        break;
+    }
+  };
+
   render() {
     const { stack, animValue } = this.state;
     return (
@@ -89,56 +172,62 @@ export class Router extends React.Component<{}, RouterState> {
               {
                 opacity: animValue.interpolate({
                   inputRange: [index - 1, index, index + 1, index + 2],
-                  outputRange: [1, 1, 0.5, 0.5],
+                  outputRange: [1, 1, 0.5, 0.5]
                 }),
                 transform: [
                   {
                     translateX: animValue.interpolate({
                       inputRange: [index - 2, index - 1, index, index + 1],
-                      outputRange: [WIDTH, WIDTH, 0, 0],
-                    }),
+                      outputRange: [WIDTH, WIDTH, 0, 0]
+                    })
                   },
                   {
                     scale: animValue.interpolate({
                       inputRange: [index - 1, index, index + 1, index + 2],
-                      outputRange: [1, 1, 0.7, 0.7],
-                    }),
-                  },
-                ],
-              },
-            ]}>
-            <ScreenTransitionContext onSharedElementsUpdated={this.onSharedElementsUpdated}>
+                      outputRange: [1, 1, 0.7, 0.7]
+                    })
+                  }
+                ]
+              }
+            ]}
+          >
+            <ScreenTransitionContext
+              onSharedElementsUpdated={this.onSharedElementsUpdated}
+            >
               {node}
             </ScreenTransitionContext>
           </Animated.View>
         ))}
         {this.renderSharedElementTransitions()}
+        {this.renderBackSwiper()}
       </View>
     );
   }
 
-  onSharedElementsUpdated = (event: ScreenTransitionContextOnSharedElementsUpdatedEvent) => {
+  onSharedElementsUpdated = (
+    event: ScreenTransitionContextOnSharedElementsUpdatedEvent
+  ) => {
     const { stack, sharedElementSources } = this.state;
     const index = stack.indexOf(event.children);
     if (index < 0) return;
     const newSharedElementSources = [...sharedElementSources];
     newSharedElementSources[index] = event.sharedElementSources;
     this.setState({
-      sharedElementSources: newSharedElementSources,
+      sharedElementSources: newSharedElementSources
     });
   };
 
   push(node: React.Node) {
-    const { stack, animValue, nextIndex, sharedElementSources } = this.state;
+    const { stack, nextIndex, sharedElementSources } = this.state;
     this.setState({
       stack: [...stack, node],
       sharedElementSources: [...sharedElementSources, {}],
-      nextIndex: nextIndex + 1,
+      nextIndex: nextIndex + 1
     });
-    Animated.timing(animValue, {
+    Animated.timing(this._animValue, {
       toValue: stack.length,
       duration: 400,
-      useNativeDriver: true,
+      useNativeDriver: true
     }).start(({ finished }) => {
       if (finished) {
         this.pruneStack(stack.length + 1);
@@ -147,15 +236,15 @@ export class Router extends React.Component<{}, RouterState> {
   }
 
   pop() {
-    const { stack, animValue, nextIndex } = this.state;
+    const { stack, nextIndex } = this.state;
     if (stack.length <= 1) return;
     this.setState({
-      nextIndex: nextIndex - 1,
+      nextIndex: nextIndex - 1
     });
-    Animated.timing(animValue, {
+    Animated.timing(this._animValue, {
       toValue: stack.length - 2,
       duration: 400,
-      useNativeDriver: true,
+      useNativeDriver: true
     }).start(({ finished }) => {
       if (finished) {
         this.pruneStack(stack.length - 1);
@@ -169,11 +258,11 @@ export class Router extends React.Component<{}, RouterState> {
       this.setState({
         stack: stack.slice(0, count),
         sharedElementSources: sharedElementSources.slice(0, count),
-        prevIndex: nextIndex,
+        prevIndex: nextIndex
       });
     } else if (nextIndex !== prevIndex) {
       this.setState({
-        prevIndex: nextIndex,
+        prevIndex: nextIndex
       });
     }
   }
