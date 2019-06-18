728e2cccf67ec5e29a316f088d46e67c6d2784b8
Hein Rutjes <hrutjes@gmail.com>
2019-06-18 08:30:03 +0200

    Added react-native-gesture-handler dep to Example app

--- Example/android/app/src/main/java/com/sharedelementexample/MainActivity.java
+++ Example/android/app/src/main/java/com/sharedelementexample/MainActivity.java
@@ -2,6 +2,12 @@ package com.sharedelementexample;
 
 import com.facebook.react.ReactActivity;
 
+//<react-native-gesture-handler>
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+//</react-native-gesture-handler>
+
 public class MainActivity extends ReactActivity {
 
     /**
@@ -12,4 +18,16 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "SharedElementExample";
     }
+
+    //<react-native-gesture-handler>
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new ReactActivityDelegate(this, getMainComponentName()) {
+            @Override
+            protected ReactRootView createRootView() {
+              return new RNGestureHandlerEnabledRootView(MainActivity.this);
+            }
+        };
+    }
+    //</react-native-gesture-handler>
 }
