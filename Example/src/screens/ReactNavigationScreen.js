d60536c81cc72e11defc410ef0d8d58c7f9e5960
Hein Rutjes <hrutjes@gmail.com>
2019-08-22 15:46:01 +0200

    Added fade-tab and tabbar-icons to react-navigation example

--- Example/src/screens/ReactNavigationScreen.js
+++ Example/src/screens/ReactNavigationScreen.js
@@ -1,5 +1,6 @@
 // @flow
 import * as React from "react";
+import { Icon } from "../components";
 import {
   createSharedElementRenderer,
   createSharedElementScene
@@ -8,13 +9,14 @@ import { createAppContainer } from "@react-navigation/native";
 import { createStackNavigator } from "react-navigation-stack";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import { MainScreen } from "./MainScreen";
-import { DummyScreen } from "./DummyScreen";
+// import { DummyScreen } from "./DummyScreen";
 import { TilesScreen } from "./TilesScreen";
 import { DetailScreen } from "./DetailScreen";
 import { PagerScreen } from "./PagerScreen";
 import { CardScreen } from "./CardScreen";
 import { TestsScreen } from "./TestsScreen";
 import { TestScreen } from "./TestScreen";
+import { fadeIn } from "../transitions";
 
 const screens = {
   Tiles: createSharedElementScene(TilesScreen),
@@ -25,13 +27,33 @@ const screens = {
   Test: createSharedElementScene(TestScreen)
 };
 
+function isTabBarVisible(navigation: any): boolean {
+  const currentRoute =
+    navigation.state.routes[navigation.state.routes.length - 1];
+  switch (currentRoute.routeName) {
+    case "Card":
+    case "Detail":
+    case "Pager":
+      return false;
+    default:
+      return true;
+  }
+}
+
 const stackNavigator = createStackNavigator(
   {
     Stack: createSharedElementScene(MainScreen),
     ...screens
   },
   {
-    initialRouteName: "Stack"
+    initialRouteName: "Stack",
+    navigationOptions: ({ navigation }) => ({
+      tabBarLabel: "Stack",
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="login" size={18} color={tintColor} />
+      ),
+      tabBarVisible: isTabBarVisible(navigation)
+    })
   }
 );
 
@@ -42,13 +64,39 @@ const modalNavigator = createStackNavigator(
   },
   {
     initialRouteName: "Modal",
-    mode: "modal"
+    mode: "modal",
+    navigationOptions: ({ navigation }) => ({
+      tabBarLabel: "Modal",
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="popup" size={18} color={tintColor} />
+      ),
+      tabBarVisible: isTabBarVisible(navigation)
+    })
+  }
+);
+
+const fadeNavigator = createStackNavigator(
+  {
+    Fade: createSharedElementScene(MainScreen),
+    ...screens
+  },
+  {
+    initialRouteName: "Fade",
+    navigationOptions: ({ navigation }) => ({
+      tabBarLabel: "Fade",
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="moon" size={18} color={tintColor} />
+      ),
+      tabBarVisible: isTabBarVisible(navigation)
+    }),
+    transitionConfig: () => fadeIn(0, true)
   }
 );
 
 export const tabNavigator = createBottomTabNavigator({
   stack: stackNavigator,
-  modal: modalNavigator
+  modal: modalNavigator,
+  fade: fadeNavigator
 });
 
 const AppContainer = createAppContainer(
