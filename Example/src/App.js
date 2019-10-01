650d0a7d50b5b45bab897647c30df5d41b4296c1
Hein Rutjes <hrutjes@gmail.com>
2019-10-01 13:58:01 +0200

    test: fixed crash in react-navigation after rn61 upgrade
    https://github.com/react-navigation/react-navigation/issues/6330

--- Example/src/App.js
+++ Example/src/App.js
@@ -1,5 +1,6 @@
 // @flow
 import * as React from "react";
+import "react-native-gesture-handler";
 import { StatusBar } from "react-native";
 import { Router, ListItem } from "./components";
 import { MainScreen } from "./screens";
