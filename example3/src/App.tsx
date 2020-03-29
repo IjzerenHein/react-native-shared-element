482518bb9b3a9413edefa71bd2e3840b9abc681c
Hein Rutjes <hrutjes@gmail.com>
2020-03-29 13:05:38 +0200

    [test] Remove test translate in test app

--- example3/src/App.tsx
+++ example3/src/App.tsx
@@ -12,7 +12,7 @@ StatusBar.setBackgroundColor("transparent");
 export default class App extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, marginTop: 0, transform: [{ translateY: 50 }] }}>
+      <View style={{ flex: 1, marginTop: 0, transform: [{ translateY: 0 }] }}>
         <Router
           initialNode={
             <MainScreen
