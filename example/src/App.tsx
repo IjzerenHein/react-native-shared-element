d86329c71e5c2d4b121efbf846baa2ddbf560a7a
Hein Rutjes <hrutjes@gmail.com>
2021-08-11 11:55:19 +0200

    [test] Use non-translucent statusbar on Android example app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -4,9 +4,11 @@ import { StatusBar, View, Platform } from "react-native";
 import { Router } from "./components";
 import { MainScreen } from "./screens";
 
+// Use non-translucent status-bar on Android on purpose.
+// This creates an additional translation and is a good
+// scenario to test and support.
 if (Platform.OS === "android") {
-  StatusBar.setTranslucent(true);
-  StatusBar.setBackgroundColor("transparent");
+  StatusBar.setTranslucent(false);
 }
 
 export default class App extends React.Component {
--- example/src/components/navBar/constants.tsx
+++ example/src/components/navBar/constants.tsx
@@ -1,3 +1,9 @@
+import { Platform } from "react-native";
 import { getStatusBarHeight } from "react-native-iphone-x-helper";
 
-export const NavBarHeight = 56 + getStatusBarHeight();
+export const NavBarHeight =
+  56 +
+  Platform.select({
+    android: 0,
+    default: getStatusBarHeight(),
+  });
