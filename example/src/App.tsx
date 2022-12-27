06abbf1d21ff07f2e78f15a086c497f6a0b9039e
Hein Rutjes <hrutjes@gmail.com>
2022-12-27 14:58:53 +0100

    [chore] Fix example app header appearance on Android

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -5,22 +5,22 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Router } from "./components";
 import { MainScreen } from "./screens";
 
-// Use non-translucent status-bar on Android on purpose.
-// This creates an additional translation and is a good
-// scenario to test and support.
+// Set this variable to true to test the statusbar offset on
+// Android. This creates an additional translation and is a
+// good scenario to test and support.
+const TEST_ANDROID_STATUSBAR_OFFSET = false;
+
 if (Platform.OS === "android") {
-  StatusBar.setTranslucent(false);
-  // StatusBar.setBackgroundColor("transparent");
+  StatusBar.setTranslucent(!TEST_ANDROID_STATUSBAR_OFFSET);
+  StatusBar.setBackgroundColor("transparent");
 }
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <SafeAreaProvider>
-        <View style={{ flex: 1, marginTop: 0, transform: [{ translateY: 0 }] }}>
-          <Router initialNode={<MainScreen />} />
-        </View>
-      </SafeAreaProvider>
-    );
-  }
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <View style={{ flex: 1, marginTop: 0, transform: [{ translateY: 0 }] }}>
+        <Router initialNode={<MainScreen />} />
+      </View>
+    </SafeAreaProvider>
+  );
 }
